2119b852b2cc740479c3ff052377efa0f4fb3f31
Anda BEN <anben@c4r9s4.wethinkcode.co.za>
2020-01-30 11:43:26 +0200

    fix me

--- startup.sh
+++ startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -fsSL https://rawgit.com/kube/42homebrew/master/install.sh | zsh
-cd /startup/
 sh python.sh

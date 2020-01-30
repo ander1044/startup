3f9bd4165897e0f5834ca7d0597f614b3d39965c
Anda BEN <anben@c4r9s4.wethinkcode.co.za>
2020-01-30 11:42:47 +0200

    fix me

---
+++ database.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+brew tap mongodb/brew
+brew install mongodb-community@4.2
+brew services start mongodb-community@4.2
+mongo
+exit
+cd /~
+mkdir dir
+cd dir
+git clone https://github.com/luckyMcHanc/matcha.git
+exit
---
+++ python.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+brew install python3
+pip3 install tornado
+pip3 install pymongo
+pip3 install bcrypt
+sh database.sh
